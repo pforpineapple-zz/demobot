127e84f06f48306ed9ff9431d856f64464d3b9ef
Anas Arifi <pforpineapple@Anass-MacBook-Pro.local>
2016-07-16 23:42:47 +0200

    initial commit

---
+++ pile_ou_face.rb
@@ -0,0 +1,38 @@
+require 'http'
+require 'json'
+require 'eventmachine'
+require 'faye/websocket'
+
+rc = HTTP.post("https://slack.com/api/rtm.start", params: {
+  token: 'xoxb-60380722211-TsDtNl9VaplKtOPUmwfo6ZKS',
+})
+
+rc = JSON.parse(rc.body)
+
+url = rc['url']
+
+EM.run do
+	ws = Faye::WebSocket::Client.new(url)
+
+	ws.on :open do
+		p [:open]
+	end
+
+	ws.on :message do |event|
+		p  [:message, JSON.parse(event.data)]
+		data = JSON.parse(event.data)
+		if data['text'] == 'hi'
+			ws.send({
+				type: 'message',
+				text: "hi <@#{data['user']}>",
+				channel: data['channel'],
+				}.to_json)
+		end
+	end
+
+  ws.on :close do |event|
+    p [:close, event.code, event.reason]
+    ws = nil
+    EM.stop
+  end
+end
